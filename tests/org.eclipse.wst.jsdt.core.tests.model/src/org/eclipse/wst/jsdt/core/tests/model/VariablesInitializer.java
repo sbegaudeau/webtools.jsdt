26dfbcfb33130e23597875cb53a1b22e8a98b645
pberkland <pberkland>
2007-02-27 17:13:30 +0000

    initial checkin, copy of org.eclipse.jdt.core.tests.model

---
+++ tests/org.eclipse.wst.jsdt.core.tests.model/src/org/eclipse/wst/jsdt/core/tests/model/VariablesInitializer.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.jsdt.core.tests.model;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.wst.jsdt.core.ClasspathVariableInitializer;
+import org.eclipse.wst.jsdt.core.JavaCore;
+import org.eclipse.wst.jsdt.core.JavaModelException;
+
+public class VariablesInitializer extends ClasspathVariableInitializer {
+
+	public static ITestInitializer initializer;
+	
+	public static interface ITestInitializer {
+		public void initialize(String variable) throws JavaModelException;
+	}
+	
+	public static void reset() {
+		initializer = null;
+		String[] varNames = JavaCore.getClasspathVariableNames();
+		try {
+			JavaCore.setClasspathVariables(varNames, new IPath[varNames.length], null);
+		} catch (JavaModelException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void setInitializer(ITestInitializer initializer) {
+		VariablesInitializer.initializer = initializer;
+	}
+	
+	public void initialize(String variable) {
+		if (initializer == null) return;
+		try {
+			initializer.initialize(variable);
+		} catch (JavaModelException e) {
+			e.printStackTrace();
+		}
+	}
+}
