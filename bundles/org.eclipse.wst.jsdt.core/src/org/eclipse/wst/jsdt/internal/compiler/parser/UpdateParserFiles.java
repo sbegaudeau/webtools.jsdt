b0ff726105c1a23738243ba3c8cfba45f8750867
pberkland <pberkland>
2007-04-13 15:01:52 +0000

    build time utility for parser files

---
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/parser/UpdateParserFiles.java
@@ -0,0 +1,20 @@
+package org.eclipse.wst.jsdt.internal.compiler.parser;
+
+import java.io.IOException;
+
+public class UpdateParserFiles {
+
+ 
+	public static void main(String[] args) throws IOException {
+		if (args.length != 2) {
+			printUsage();
+			return;
+		}
+		Parser.buildFilesFromLPG(args[0], args[1]);
+	}
+	
+	public static void printUsage() {
+		System.out.println("Usage: UpdateParserFiles <path to javadcl.java> <path to javahdr.java>");
+		System.out.println("e.g. UpdateParserFiles c:/javadcl.java c:/javahdr.java");
+	}
+}
