f79dd7f3f8782b88f20d084d112f7cb5aa08a370
pberkland <pberkland>
2007-07-24 01:57:42 +0000

    bug 196888 - static methods not handled correctly

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/lookup/MethodScope.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/lookup/MethodScope.java
@@ -299,6 +299,8 @@ public class MethodScope extends BlockScope {
 		this.referenceContext = method;
 		method.scope = this;
 		int modifiers = method.modifiers | ExtraCompilerModifiers.AccUnresolved | ClassFileConstants.AccPublic;
+		if (method.inferredMethod.isStatic)
+			modifiers|= ClassFileConstants.AccStatic;
 		if (method.isConstructor() || isConstructor) {
 			if (method.isDefaultConstructor() || isConstructor)
 				modifiers |= ExtraCompilerModifiers.AccIsDefaultConstructor;
