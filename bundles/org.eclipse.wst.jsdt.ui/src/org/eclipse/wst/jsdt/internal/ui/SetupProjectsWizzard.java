d79c9880d51fde3c14430f4e3d05e779ffca1685
bchilds <bchilds>
2007-09-12 18:40:20 +0000

    Support for adding JSDT support to any project.

---
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/SetupProjectsWizzard.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package org.eclipse.wst.jsdt.internal.ui;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Vector;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+
+
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.wst.jsdt.core.JavaCore;
+import org.eclipse.wst.jsdt.core.UnimplementedException;
+import org.eclipse.wst.jsdt.ui.project.JsNature;
+
+/**
+ * @author childsb
+ *
+ */
+public class SetupProjectsWizzard implements IObjectActionDelegate, IActionDelegate {
+	
+	Object[] fTarget;
+
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		throw new UnimplementedException("setActivePart(IAction action, IWorkbenchPart targetPart)");
+	}
+
+	public void run(IAction action) {
+		if(fTarget==null) return;
+		
+		for(int i=0;i<fTarget.length;i++) {
+			if(fTarget[i] instanceof IProject) {
+				IProject project = (IProject)fTarget[i];
+				
+					if(!JsNature.hasNature(project)) {
+						JsNature nature = new JsNature(project,null);
+						try {
+							nature.configure();
+						} catch (CoreException ex) {
+							// TODO Auto-generated catch block
+							ex.printStackTrace();
+						}
+					}
+				
+			}
+		}
+		
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		
+		if(selection instanceof StructuredSelection) {
+			fTarget = ((StructuredSelection)selection).toArray();
+		}else {
+			fTarget = null;
+		}
+	}
+	
+
+}
