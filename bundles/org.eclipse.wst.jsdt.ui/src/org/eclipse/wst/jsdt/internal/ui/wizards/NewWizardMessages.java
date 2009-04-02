967df5b11cff130b49d5b83dc2024918ef55fa0b
nitind <nitind>
2009-04-02 05:33:06 +0000

    [235676] NLS missing message: GenerateHashCodeEqualsDialog_instanceof_button in: org.eclipse.wst.jsdt.internal.ui.JavaUIMessages

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/wizards/NewWizardMessages.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/wizards/NewWizardMessages.java
@@ -157,10 +157,6 @@ public final class NewWizardMessages extends NLS {
 	public static String NewTypeWizardPage_operationdesc;
 	public static String NewTypeWizardPage_error_uri_location_unkown;
 	
-	//public static String OutputLocation_DotAsLocation;
-	//public static String OutputLocation_SettingsAsLocation;
-	//public static String OutputLocationDialog_removeProjectFromBP;
-	
 	public static String RemoveFromBuildpathAction_ErrorTitle;
 	
 	public static String SuperInterfaceSelectionDialog_addButton_label;
