bef25932ef5098f6953a23613087b24105edf481
nitind <nitind>
2009-04-21 21:04:07 +0000

    [251280] It's a PixelConverter party and everyone's invited

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/util/PixelConverter.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/util/PixelConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,53 +10,19 @@
  *******************************************************************************/
 package org.eclipse.wst.jsdt.internal.ui.util;
 
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.FontMetrics;
-import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.widgets.Control;
 
-public class PixelConverter {
-	
-	private final FontMetrics fFontMetrics;
-	
+/**
+ * @deprecated - use {@link org.eclipse.jface.layout.PixelConverter}
+ */
+public class PixelConverter extends org.eclipse.jface.layout.PixelConverter {
+
 	public PixelConverter(Control control) {
-		this(control.getFont());
+		super(control);
 	}
-	
+
 	public PixelConverter(Font font) {
-		GC gc = new GC(font.getDevice());
-		gc.setFont(font);
-		fFontMetrics= gc.getFontMetrics();
-		gc.dispose();
-	}
-	
-	/*
-	 * see org.eclipse.jface.dialogs.DialogPage#convertHeightInCharsToPixels(int)
-	 */
-	public int convertHeightInCharsToPixels(int chars) {
-		return Dialog.convertHeightInCharsToPixels(fFontMetrics, chars);
-	}
-
-	/*
-	 * see org.eclipse.jface.dialogs.DialogPage#convertHorizontalDLUsToPixels(int)
-	 */
-	public int convertHorizontalDLUsToPixels(int dlus) {
-		return Dialog.convertHorizontalDLUsToPixels(fFontMetrics, dlus);
-	}
-
-	/*
-	 * see org.eclipse.jface.dialogs.DialogPage#convertVerticalDLUsToPixels(int)
-	 */
-	public int convertVerticalDLUsToPixels(int dlus) {
-		return Dialog.convertVerticalDLUsToPixels(fFontMetrics, dlus);
-	}
-	
-	/*
-	 * see org.eclipse.jface.dialogs.DialogPage#convertWidthInCharsToPixels(int)
-	 */
-	public int convertWidthInCharsToPixels(int chars) {
-		return Dialog.convertWidthInCharsToPixels(fFontMetrics, chars);
+		super(font);
 	}	
-
 }
