ee1f8581478799f86f006aad0978a3363d32f6e6
cjaun <cjaun>
2009-06-15 21:18:28 +0000

    [233599] Compiler warnings in JSDT plugins

--- bundles/org.eclipse.wst.jsdt.support.ie/src/org/eclipse/wst/jsdt/jsdoc/GenerateJsDoc.java
+++ bundles/org.eclipse.wst.jsdt.support.ie/src/org/eclipse/wst/jsdt/jsdoc/GenerateJsDoc.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -49,7 +49,6 @@ public class GenerateJsDoc {
 	}
 
 	public static void main(String[] args) {
-		ElementInfo tempElement = new ElementInfo(null,null);
 
 		ElementInfo[] tops = source.getTopObjects();
 		ArrayList allFoundNodes= new ArrayList();
@@ -110,7 +109,6 @@ public class GenerateJsDoc {
 			System.out.println("Writing Collections and Events....");
 
 			tops = (ElementInfo[])allFoundNodes.toArray(new ElementInfo[allFoundNodes.size()]);
-			tempElement=null;
 		}
 
 	}
