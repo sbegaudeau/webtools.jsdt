33cf93f36f9e7d29c682d2e74b4fcff2c5fad473
mrennie <mrennie>
2010-04-07 16:27:59 +0000

    Bug 307912 -  The 'Scripts' object in the Debug view should support ITerminate

--- bundles/org.eclipse.wst.jsdt.debug.core/src/org/eclipse/wst/jsdt/debug/internal/core/model/ScriptGroup.java
+++ bundles/org.eclipse.wst.jsdt.debug.core/src/org/eclipse/wst/jsdt/debug/internal/core/model/ScriptGroup.java
@@ -12,6 +12,8 @@ package org.eclipse.wst.jsdt.debug.internal.core.model;
 
 import java.util.List;
 
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.model.ITerminate;
 import org.eclipse.wst.jsdt.debug.core.model.IJavaScriptDebugTarget;
 import org.eclipse.wst.jsdt.debug.core.model.IScriptGroup;
 
@@ -21,7 +23,7 @@ import org.eclipse.wst.jsdt.debug.core.model.IScriptGroup;
  * 
  * @since 1.0
  */
-public class ScriptGroup extends JavaScriptDebugElement implements IScriptGroup {
+public class ScriptGroup extends JavaScriptDebugElement implements IScriptGroup, ITerminate {
 	
 	/**
 	 * Constructor
@@ -52,5 +54,26 @@ public class ScriptGroup extends JavaScriptDebugElement implements IScriptGroup 
 	 */
 	public int hashCode() {
 		return getJavaScriptDebugTarget().hashCode();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.core.model.ITerminate#canTerminate()
+	 */
+	public boolean canTerminate() {
+		return getJavaScriptDebugTarget().canTerminate();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.core.model.ITerminate#isTerminated()
+	 */
+	public boolean isTerminated() {
+		return getJavaScriptDebugTarget().isTerminated();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.core.model.ITerminate#terminate()
+	 */
+	public void terminate() throws DebugException {
+		getJavaScriptDebugTarget().terminate();
 	}
 }
