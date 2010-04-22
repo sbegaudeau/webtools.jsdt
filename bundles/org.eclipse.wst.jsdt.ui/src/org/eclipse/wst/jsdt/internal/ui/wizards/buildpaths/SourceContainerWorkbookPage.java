299c982b75311794656c62d72fd754a5ed9a29ab
nitind <nitind>
2010-04-22 01:43:09 +0000

    [nobug] disable unhandled Add Variable button

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/wizards/buildpaths/SourceContainerWorkbookPage.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/wizards/buildpaths/SourceContainerWorkbookPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -171,6 +171,7 @@ public class SourceContainerWorkbookPage extends BuildPathBasePage {
 		
 		fFoldersList.setViewerComparator(new CPListElementSorter());
 		fFoldersList.enableButton(IDX_EDIT, false);
+		fFoldersList.enableButton(IDX_ADDVAR, false);
 		
 //		fUseFolderOutputs= new SelectionButtonDialogField(SWT.CHECK);
 //		fUseFolderOutputs.setSelection(false);
