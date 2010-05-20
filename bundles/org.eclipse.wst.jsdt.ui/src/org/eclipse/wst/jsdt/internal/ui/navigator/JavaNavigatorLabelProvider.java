1c1acd9f9e8aad8e944a9162a90e2281e27bac76
nitind <nitind>
2010-05-20 01:05:59 +0000

    [313556] [project explorer] Labels always assume groups are enabled

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/navigator/JavaNavigatorLabelProvider.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/navigator/JavaNavigatorLabelProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -140,7 +140,7 @@ public class JavaNavigatorLabelProvider implements ICommonLabelProvider {
 	}
 
 	public String getText(Object element) {
-		if (element instanceof IJavaScriptElement) {
+		if (!delegeteLabelProvider.isFlatLayout() && element instanceof IJavaScriptElement) {
 			switch (((IJavaScriptElement) element).getElementType()) {
 				case IJavaScriptElement.TYPE :
 				case IJavaScriptElement.METHOD :
