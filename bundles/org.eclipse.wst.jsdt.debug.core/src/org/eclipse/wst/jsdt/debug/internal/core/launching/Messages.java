890263e30de5612ab8157635fd343e35a9fb7320
mrennie <mrennie>
2010-06-29 20:33:40 +0000

    Bug 318380 - JavaScript launch delegate does not report progress while launching

--- bundles/org.eclipse.wst.jsdt.debug.core/src/org/eclipse/wst/jsdt/debug/internal/core/launching/Messages.java
+++ bundles/org.eclipse.wst.jsdt.debug.core/src/org/eclipse/wst/jsdt/debug/internal/core/launching/Messages.java
@@ -17,8 +17,13 @@ import org.eclipse.osgi.util.NLS;
  */
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.wst.jsdt.debug.internal.core.launching.messages"; //$NON-NLS-1$
+	public static String acquiring_connector;
 	public static String argument_map_null;
+	public static String attaching_to_vm;
 	public static String could_not_locate_connector;
+	public static String creating_debug_target;
+	public static String launching_js_debug_delegate;
+	public static String waiting_for_vm_to_connect;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
--- bundles/org.eclipse.wst.jsdt.debug.core/src/org/eclipse/wst/jsdt/debug/internal/core/launching/RemoteJavaScriptLaunchDelegate.java
+++ bundles/org.eclipse.wst.jsdt.debug.core/src/org/eclipse/wst/jsdt/debug/internal/core/launching/RemoteJavaScriptLaunchDelegate.java
@@ -17,6 +17,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
@@ -43,37 +44,62 @@ public class RemoteJavaScriptLaunchDelegate extends LaunchConfigurationDelegate 
 	 * @see org.eclipse.debug.core.model.ILaunchConfigurationDelegate#launch(org. eclipse.debug.core.ILaunchConfiguration, java.lang.String, org.eclipse.debug.core.ILaunch, org.eclipse.core.runtime.IProgressMonitor)
 	 */
 	public void launch(ILaunchConfiguration configuration, String mode, final ILaunch launch, IProgressMonitor monitor) throws CoreException {
-		String name = configuration.getAttribute(ILaunchConstants.CONNECTOR_ID, (String) null);
-		
-		Map argmap = configuration.getAttribute(ILaunchConstants.ARGUMENT_MAP, (Map) null);
-		if (argmap == null) {
-			Status status = new Status(
-					IStatus.ERROR, 
-					JavaScriptDebugPlugin.PLUGIN_ID, 
-					Messages.argument_map_null);
-			throw new CoreException(status);
-		}
-		Connector connector = JavaScriptDebugPlugin.getConnectionsManager().getConnector(name);
-		VirtualMachine vm;
+		SubMonitor localmonitor = SubMonitor.convert(monitor, Messages.launching_js_debug_delegate, 16);
 		try {
-			if(connector instanceof AttachingConnector) {
-				vm = ((AttachingConnector)connector).attach(argmap);
-			}
-			else if(connector instanceof ListeningConnector) {
-				vm = ((ListeningConnector)connector).accept(argmap);
-			}
-			else {
+			String name = configuration.getAttribute(ILaunchConstants.CONNECTOR_ID, (String) null);
+			Map argmap = configuration.getAttribute(ILaunchConstants.ARGUMENT_MAP, (Map) null);
+			if (argmap == null) {
 				Status status = new Status(
 						IStatus.ERROR, 
 						JavaScriptDebugPlugin.PLUGIN_ID, 
-						NLS.bind(Messages.could_not_locate_connector, new String[] {name}));
+						Messages.argument_map_null);
 				throw new CoreException(status);
 			}
-		} catch (IOException e) {
-			Status status = new Status(IStatus.ERROR, JavaScriptDebugPlugin.PLUGIN_ID, "Error occured while launching", e); //$NON-NLS-1$
-			throw new CoreException(status);
+			if(localmonitor.isCanceled()) {
+				return;
+			}
+			localmonitor.worked(4);
+			localmonitor.subTask(Messages.acquiring_connector);
+			Connector connector = JavaScriptDebugPlugin.getConnectionsManager().getConnector(name);
+			VirtualMachine vm;
+			try {
+				if(localmonitor.isCanceled()) {
+					return;
+				}
+				if(connector instanceof AttachingConnector) {
+					localmonitor.subTask(Messages.attaching_to_vm);
+					vm = ((AttachingConnector)connector).attach(argmap);
+				}
+				else if(connector instanceof ListeningConnector) {
+					localmonitor.subTask(Messages.waiting_for_vm_to_connect);
+					vm = ((ListeningConnector)connector).accept(argmap);
+				}
+				else {
+					Status status = new Status(
+							IStatus.ERROR, 
+							JavaScriptDebugPlugin.PLUGIN_ID, 
+							NLS.bind(Messages.could_not_locate_connector, new String[] {name}));
+					throw new CoreException(status);
+				}
+				localmonitor.worked(4);
+			} catch (IOException e) {
+				Status status = new Status(IStatus.ERROR, JavaScriptDebugPlugin.PLUGIN_ID, "Error occured while launching", e); //$NON-NLS-1$
+				throw new CoreException(status);
+			}
+			if(localmonitor.isCanceled()) {
+				return;
+			}
+			localmonitor.worked(4);
+			localmonitor.subTask(Messages.creating_debug_target);
+			JavaScriptDebugTarget target = new JavaScriptDebugTarget(vm, null, launch, vm.name(), true, true);
+			if(localmonitor.isCanceled()) {
+				return;
+			}
+			localmonitor.worked(4);
+			launch.addDebugTarget(target);
 		}
-		JavaScriptDebugTarget target = new JavaScriptDebugTarget(vm, null, launch, vm.name(), true, true);
-		launch.addDebugTarget(target);
+		finally {
+			localmonitor.done();
+		}
 	}
 }
