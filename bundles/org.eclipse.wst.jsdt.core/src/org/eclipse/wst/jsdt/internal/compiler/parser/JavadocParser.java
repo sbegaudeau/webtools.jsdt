11befaf81ab352c24c1e6cb5d1dc9242c794d603
nitind <nitind>
2010-07-31 05:48:12 +0000

    [nobug] @extends should imply @constructor

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/parser/JavadocParser.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/parser/JavadocParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -743,6 +743,8 @@ public class JavadocParser extends AbstractCommentParser {
 
 	private boolean parseExtends() throws InvalidInputException {
 		this.extendsType=(TypeReference)parseQualifiedName(true);
+		if (extendsType != null)
+			this.isConstructor = true;
 		return this.extendsType!=null;
 	}
 
