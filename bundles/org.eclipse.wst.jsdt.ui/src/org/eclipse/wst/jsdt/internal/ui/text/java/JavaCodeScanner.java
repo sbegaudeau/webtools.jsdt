4924f164764f071fbdd43c9ea98449c7980f6db4
cjaun <cjaun>
2010-08-09 20:43:21 +0000

    [321260] with keyword not highlighted

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/text/java/JavaCodeScanner.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/text/java/JavaCodeScanner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -370,7 +370,7 @@ public final class JavaCodeScanner extends AbstractJavaScanner {
 		"static", "super", "switch", "synchronized", //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$
 		"this", "throw", "throws", "transient", "try","typeof",  //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$
 		"var", "volatile", //$NON-NLS-1$ //$NON-NLS-2$
-		"while" //$NON-NLS-1$
+		"while", "with" //$NON-NLS-1$ //$NON-NLS-2$
 	};
 
 //	private static final String INTERFACE= "interface";  //$NON-NLS-1$
