8526894e4806e4a601314b3f44aa1c9ef1f5424a
nitind <nitind>
2010-08-09 21:54:47 +0000

    [nobug] fix testGetJavaScriptLikeExtensions01 and testGetJavaScriptLikeExtensions02

--- tests/org.eclipse.wst.jsdt.core.tests.model/src/org/eclipse/wst/jsdt/core/tests/model/JavaScriptLikeExtensionsTests.java
+++ tests/org.eclipse.wst.jsdt.core.tests.model/src/org/eclipse/wst/jsdt/core/tests/model/JavaScriptLikeExtensionsTests.java
@@ -37,8 +37,11 @@ public class JavaScriptLikeExtensionsTests extends ModifyingResourceTests {
 	 * Ensures that the known JavaScript-like extensions are correct.
 	 */
 	public void testGetJavaScriptLikeExtensions01() {
+		// this test plug-in now contributes two for another test
 		assertSortedStringsEqual(
 			"Unexpected file extensions",
+			"bar\n"+
+			"foo\n"+
 			"js\n",
 			JavaScriptCore.getJavaScriptLikeExtensions()
 		);
@@ -54,7 +57,9 @@ public class JavaScriptLikeExtensionsTests extends ModifyingResourceTests {
 				javaContentType.addFileSpec("abc", IContentType.FILE_EXTENSION_SPEC);
 			assertSortedStringsEqual(
 				"Unexpected file extensions",
-				"abc\n" + 
+				"abc\n"+
+				"bar\n"+
+				"foo\n"+
 				"js\n",
 				JavaScriptCore.getJavaScriptLikeExtensions()
 			);
