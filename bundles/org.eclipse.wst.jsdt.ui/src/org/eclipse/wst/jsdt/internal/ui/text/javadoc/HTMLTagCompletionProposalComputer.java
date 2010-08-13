2c920ed292be45282b5fc22197374e5db3008ac5
cjaun <cjaun>
2010-08-13 20:19:20 +0000

    [277557] [exception] [content assist] jsdoc suggestion fails when file opened with "File/Open File..."

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/text/javadoc/HTMLTagCompletionProposalComputer.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/text/javadoc/HTMLTagCompletionProposalComputer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -131,7 +131,7 @@ public class HTMLTagCompletionProposalComputer implements IJavaCompletionProposa
 		IEditorInput editorInput= new FileEditorInput((IFile) cu.getResource());
 		fDocument= JavaScriptUI.getDocumentProvider().getDocument(editorInput);
 		if (fDocument == null) {
-			return null;
+			return Collections.EMPTY_LIST;
 		}
 
 		try {
