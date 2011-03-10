2bbf0843ab9e0f677b280d0e14a05ba90adaa621
cjaun <cjaun>
2011-03-10 16:21:37 +0000

    [339545] NPE in JavaSearchScope - line 210

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/core/search/JavaSearchScope.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/core/search/JavaSearchScope.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -207,6 +207,8 @@ public void add(IJavaScriptElement element) throws JavaScriptModelException {
 			IPath rootPath = root.getPath();
 
 			IIncludePathEntry entry = root.getResolvedIncludepathEntry();
+			if(entry == null)
+				break;
 			IPath[] exclusionsPaths = entry.getExclusionPatterns();
 
 			if(exclusionsPaths!=null &&  exclusionsPaths.length>0)
