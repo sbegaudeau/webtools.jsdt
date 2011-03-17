624d016dcf1c1a9e86496c92fd6fdc7ca15f1013
nitind <nitind>
2011-03-17 02:32:54 +0000

    [340245] JavaScript indexing thread is indexing data outside of workspace

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/core/search/SearchParticipant.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/core/search/SearchParticipant.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -209,7 +209,7 @@ public abstract class SearchParticipant {
 		if (file instanceof IResource) {
 			containerPath = ((IResource)file).getProject().getFullPath();
 		} else if (file == null) {
-			containerPath = documentPath.removeLastSegments(documentPath.segmentCount()-1);
+			containerPath = documentPath.removeLastSegments(1);
 		}
 		IndexManager manager = JavaModelManager.getJavaModelManager().getIndexManager();
 		// TODO (frederic) should not have to create index manually, should expose API that recreates index instead
