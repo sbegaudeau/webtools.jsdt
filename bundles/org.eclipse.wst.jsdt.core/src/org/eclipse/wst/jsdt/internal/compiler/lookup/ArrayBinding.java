19278ab8ee45544c1ba1ca053e59ea9729cf2577
cjaun <cjaun>
2011-03-30 19:23:53 +0000

    [341358] error pop up on JavaScript proposal

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/lookup/ArrayBinding.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/lookup/ArrayBinding.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -194,7 +194,9 @@ public char[] sourceName() {
 	return CharOperation.concat(leafComponentType.sourceName(), brackets);
 }
 public ReferenceBinding superclass() {
-	return referenceBinding.superclass();
+	if(referenceBinding != null)
+		return referenceBinding.superclass();
+	return null;
 }
 public void swapUnresolved(UnresolvedReferenceBinding unresolvedType, ReferenceBinding resolvedType, LookupEnvironment env) {
 	if (this.leafComponentType == unresolvedType) {
