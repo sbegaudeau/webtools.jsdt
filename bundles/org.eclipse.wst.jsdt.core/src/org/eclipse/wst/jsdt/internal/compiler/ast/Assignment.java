9fda199314847610fdabb20be0cfe9836ebfb53d
cjaun <cjaun>
2011-07-01 14:42:28 +0000

    [350537] [validation] Type mismatch when rewriting an object

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/ast/Assignment.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/ast/Assignment.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -197,7 +197,7 @@ public TypeBinding resolveType(BlockScope scope) {
 	}
 	if (rhsType.isFunctionType() && this.lhs.isTypeReference())
 		return lhsType;
-	scope.problemReporter().typeMismatchError(rhsType, lhsType, this.expression);
+	//scope.problemReporter().typeMismatchError(rhsType, lhsType, this.expression);
 	return lhsType;
 }
 
