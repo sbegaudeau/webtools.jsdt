d3ff8d0fd48453b5b778ff373048e49c1acd1692
mrennie <mrennie>
2012-01-25 17:59:56 +0000

    [nobug] updated since tag

--- bundles/org.eclipse.wst.jsdt.debug.core/src/org/eclipse/wst/jsdt/debug/core/model/ScriptResolver.java
+++ bundles/org.eclipse.wst.jsdt.debug.core/src/org/eclipse/wst/jsdt/debug/core/model/ScriptResolver.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
+ * Copyright (c) 2011, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,7 +19,7 @@ import org.eclipse.wst.jsdt.debug.core.jsdi.ScriptReference;
  * <br><br>
  * Clients must extend this class and not directly implement the interface {@link IScriptResolver}
  * 
- * @since 3.4
+ * @since 2.1
  */
 public abstract class ScriptResolver implements IScriptResolver {
 
