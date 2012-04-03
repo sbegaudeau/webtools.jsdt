5886bfb3f5c22cad75175fa360fd9ea6585ec82f
mrennie <mrennie>
2012-04-03 15:11:26 +0000

    Bug 375958 - RemoteJavaScriptLaunchDelegate displays error about Firefox

--- bundles/org.eclipse.wst.jsdt.debug.core/src/org/eclipse/wst/jsdt/debug/internal/core/launching/RemoteJavaScriptLaunchDelegate.java
+++ bundles/org.eclipse.wst.jsdt.debug.core/src/org/eclipse/wst/jsdt/debug/internal/core/launching/RemoteJavaScriptLaunchDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -152,7 +152,7 @@ public class RemoteJavaScriptLaunchDelegate extends LaunchConfigurationDelegate 
 				return;
 			}
 			if(runnable.vm == null) {
-				Status status = new Status(IStatus.ERROR, JavaScriptDebugPlugin.PLUGIN_ID, "Failed to connect to Firefox", runnable.exception); //$NON-NLS-1$
+				Status status = new Status(IStatus.ERROR, JavaScriptDebugPlugin.PLUGIN_ID, "Failed to connect to virtual machine", runnable.exception); //$NON-NLS-1$
 				throw new CoreException(status);
 			}
 			vm = runnable.vm;
