630bce56492eac450c05d467d1eecb062f887a85
Mike Rennie <Michael_Rennie@ca.ibm.com>
2013-04-02 13:23:27 -0500

    [nobug] configure PDE / API tools project specific settings

--- development/org.eclipse.wst.jsdt.debug.node/src/org/eclipse/wst/jsdt/debug/internal/node/NodePlugin.java
+++ development/org.eclipse.wst.jsdt.debug.node/src/org/eclipse/wst/jsdt/debug/internal/node/NodePlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
+ * Copyright (c) 2011, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package org.eclipse.wst.jsdt.debug.node;
+package org.eclipse.wst.jsdt.debug.internal.node;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
