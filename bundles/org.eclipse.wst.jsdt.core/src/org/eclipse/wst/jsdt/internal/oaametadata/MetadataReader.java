a1079c3c41680293060bb3793eb9802aa7517a81
Chris Jaun <cmjaun@us.ibm.com>
2013-06-28 10:52:03 -0400

    Null check to fix Bug 402623

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/oaametadata/MetadataReader.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/oaametadata/MetadataReader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2011 IBM Corporation and others.
+ * Copyright (c) 2005, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -722,7 +722,10 @@ public class MetadataReader extends DefaultHandler implements IOAAMetaDataConsta
 
 	
 	private ArrayList getCollection(String tagClass) {
-		ArrayList list = (ArrayList)this.collections.get(tagClass);
+		ArrayList list = null;
+		if(this.collections != null) {
+			list = (ArrayList)this.collections.get(tagClass);
+		}
 		if (list==null)
 			list=EMPTY_LIST;
 		return list;
