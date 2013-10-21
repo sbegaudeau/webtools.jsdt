d35ac64da4550ccb87943f318bebdf5f9f7ac3ee
vrubezhny <vrubezhny@exadel.com>
2013-10-21 17:03:04 +0400

    Bug 351713 - [refactoring] Change Function Signature broken. Cannot use at all.
    NPE is fixed
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/corext/refactoring/structure/ChangeSignatureRefactoring.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/corext/refactoring/structure/ChangeSignatureRefactoring.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -485,7 +485,7 @@ public class ChangeSignatureRefactoring extends ScriptableRefactoring implements
 			result.addFatalError(msg);
 			return;
 		}
-		if (fMethodName.equals(fMethod.getDeclaringType().getElementName())) {
+		if (fMethod.getDeclaringType() != null && fMethodName.equals(fMethod.getDeclaringType().getElementName())) {
 			String msg= RefactoringCoreMessages.ChangeSignatureRefactoring_constructor_name; 
 			result.addWarning(msg);
 		}
