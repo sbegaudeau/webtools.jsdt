b3c00d61105146af82a15184ef93599b8810e49c
Victor V Rubezhny <vrubezhny@exadel.com>
2013-10-29 14:58:05 -0400

    [420086] Copy text doesn't work from read-only javascript file

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/javaeditor/CompilationUnitEditor.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/javaeditor/CompilationUnitEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -159,7 +159,7 @@ public class CompilationUnitEditor extends JavaEditor implements IJavaReconcilin
 		 */
 		public void doOperation(int operation) {
 
-			if (getTextWidget() == null || !isEditorInputModifiable())
+			if (getTextWidget() == null)
 				return;
 
 			switch (operation) {
